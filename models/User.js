7b156a1356823ba934de89d4d1ad5a68a95f82a6
seungwan Ham <seungwan7511@gmail.com>
2022-05-13 19:57:38 +0900

    error fix

--- models/User.js
+++ models/User.js
@@ -1,97 +1,122 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 // schema
-const userSchema = mongoose.Schema({
-  username:{
-    type:String,
-    required:[true,'사용자 명은 필수입니다.'],
-    match:[/^.{3,12}$/,'3글자 이상 12글자 이하 문자로 입력바랍니다.'],
-    trim:true,
-    unique:true
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "사용자 명은 필수입니다."],
+      match: [/^.{3,12}$/, "3글자 이상 12글자 이하 문자로 입력바랍니다."],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "비밀번호는 필수입니다."],
+      select: false,
+    },
+    name: {
+      type: String,
+      required: [true, "이름은 필수입니다."],
+      match: [/^.{3,12}$/, "3글자 이상 12글자 이하 문자로 입력바랍니다."],
+      trim: true,
+    },
+    email: {
+      type: String,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "유효하지 않은 이메일 입니다.",
+      ],
+      trim: true,
+    },
   },
-  password:{
-    type:String,
-    required:[true,'비밀번호는 필수입니다.'],
-    select:false
-  },
-  name:{
-    type:String,
-    required:[true,'이름은 필수입니다.'],
-    match:[/^.{4,12}$/,'3글자 이상 12글자 이하 문자로 입력바랍니다.'],
-    trim:true
-  },
-  email:{
-    type:String,
-    match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,'유효하지 않은 이메일 입니다.'],
-    trim:true
+  {
+    toObject: { virtuals: true },
   }
-},{
-  toObject:{virtuals:true}
-});
+);
 
 // virtuals
-userSchema.virtual('passwordConfirmation')
-  .get(function(){ return this._passwordConfirmation; })
-  .set(function(value){ this._passwordConfirmation=value; });
+userSchema
+  .virtual("passwordConfirmation")
+  .get(function () {
+    return this._passwordConfirmation;
+  })
+  .set(function (value) {
+    this._passwordConfirmation = value;
+  });
 
-userSchema.virtual('originalPassword')
-  .get(function(){ return this._originalPassword; })
-  .set(function(value){ this._originalPassword=value; });
+userSchema
+  .virtual("originalPassword")
+  .get(function () {
+    return this._originalPassword;
+  })
+  .set(function (value) {
+    this._originalPassword = value;
+  });
 
-userSchema.virtual('currentPassword')
-  .get(function(){ return this._currentPassword; })
-  .set(function(value){ this._currentPassword=value; });
+userSchema
+  .virtual("currentPassword")
+  .get(function () {
+    return this._currentPassword;
+  })
+  .set(function (value) {
+    this._currentPassword = value;
+  });
 
-userSchema.virtual('newPassword')
-  .get(function(){ return this._newPassword; })
-  .set(function(value){ this._newPassword=value; });
+userSchema
+  .virtual("newPassword")
+  .get(function () {
+    return this._newPassword;
+  })
+  .set(function (value) {
+    this._newPassword = value;
+  });
 
 // password validation
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
-const passwordRegexErrorMessage = '영문과 숫자의 조합으로 최소 8자 이상이어야 합니다.';
-userSchema.path('password').validate(function(v) {
+const passwordRegexErrorMessage =
+  "영문과 숫자의 조합으로 최소 8자 이상이어야 합니다.";
+userSchema.path("password").validate(function (v) {
   var user = this;
 
   // create user
-  if(user.isNew){
-    if(!user.passwordConfirmation){
-      user.invalidate('비밀번호 확인', '비밀번호 확인이 필요합니다.');
+  if (user.isNew) {
+    if (!user.passwordConfirmation) {
+      user.invalidate("비밀번호 확인", "비밀번호 확인이 필요합니다.");
     }
 
-    if(!passwordRegex.test(user.password)){
-      user.invalidate('password', passwordRegexErrorMessage);
-    }
-    else if(user.password !== user.passwordConfirmation) {
-      user.invalidate('비밀번호 확인', '비밀번호가 일치하지 않습니다.');
+    if (!passwordRegex.test(user.password)) {
+      user.invalidate("password", passwordRegexErrorMessage);
+    } else if (user.password !== user.passwordConfirmation) {
+      user.invalidate("비밀번호 확인", "비밀번호가 일치하지 않습니다.");
     }
   }
 
   // update user
-  if(!user.isNew){
-    if(!user.currentPassword){
-      user.invalidate('현재 비밀번호', '현재 비밀번호가 필요합니다.');
-    }
-    else if(!bcrypt.compareSync(user.currentPassword, user.originalPassword)){
-      user.invalidate('현재 비밀번호', '현재 비밀번호가 잘못되었습니다.');
+  if (!user.isNew) {
+    if (!user.currentPassword) {
+      user.invalidate("현재 비밀번호", "현재 비밀번호가 필요합니다.");
+    } else if (
+      !bcrypt.compareSync(user.currentPassword, user.originalPassword)
+    ) {
+      user.invalidate("현재 비밀번호", "현재 비밀번호가 잘못되었습니다.");
     }
 
-    if(user.newPassword && !passwordRegex.test(user.newPassword)){
+    if (user.newPassword && !passwordRegex.test(user.newPassword)) {
       user.invalidate("새 비밀번호", passwordRegexErrorMessage);
-    }
-    else if(user.newPassword !== user.passwordConfirmation) {
-      user.invalidate('비밀번호 확인', '비밀번호 확인이 일치하지 않습니다.');
+    } else if (user.newPassword !== user.passwordConfirmation) {
+      user.invalidate("비밀번호 확인", "비밀번호 확인이 일치하지 않습니다.");
     }
   }
 });
 
 // hash password
-userSchema.pre('save', function (next){
+userSchema.pre("save", function (next) {
   var user = this;
-  if(!user.isModified('password')){
+  if (!user.isModified("password")) {
     return next();
-  }
-  else {
+  } else {
     user.password = bcrypt.hashSync(user.password);
     return next();
   }
@@ -100,9 +125,9 @@ userSchema.pre('save', function (next){
 // model methods
 userSchema.methods.authenticate = function (password) {
   var user = this;
-  return bcrypt.compareSync(password,user.password);
+  return bcrypt.compareSync(password, user.password);
 };
 
 // model & export
-const User = mongoose.model('user',userSchema);
+const User = mongoose.model("user", userSchema);
 module.exports = User;
--- util.js
+++ util.js
@@ -2,59 +2,81 @@ var util = {};
 
 util.parseError = function (errors) {
   var parsed = {};
-  if (errors.name == 'ValidationError') {
+  if (errors.name == "ValidationError") {
     for (var name in errors.errors) {
       var validationError = errors.errors[name];
       parsed[name] = { message: validationError.message };
     }
-  }
-  else if (errors.code == '11000' && errors.errmsg.indexOf('username') > 0) {
-    parsed.username = { message: 'This username already exists!' };
-  }
-  else {
+  } else if (errors.code == "11000" && errors.errmsg.indexOf("username") > 0) {
+    parsed.username = { message: "이 사용자명은 이미 존재합니다." };
+  } else {
     parsed.unhandled = JSON.stringify(errors);
   }
   return parsed;
-}
+};
 
 util.isLoggedin = function (req, res, next) {
   if (req.isAuthenticated()) {
     next();
+  } else {
+    req.flash("errors", { login: "로그인이 필요합니다." });
+    res.redirect("/login");
   }
-  else {
-    req.flash('errors', { login: 'Please login first' });
-    res.redirect('/login');
-  }
-}
+};
 
 util.noPermission = function (req, res) {
-  req.flash('errors', { login: "You don't have permission" });
+  req.flash("errors", { login: "접근이 제한된 사용자입니다." });
   req.logout();
-  res.redirect('/login');
-}
+  res.redirect("/login");
+};
 
 util.getPostQueryString = function (req, res, next) {
-  res.locals.getPostQueryString = function (isAppended = false, overwrites = {}) {
-    var queryString = '';
+  res.locals.getPostQueryString = function (
+    isAppended = false,
+    overwrites = {}
+  ) {
+    var queryString = "";
     var queryArray = [];
-    var page = overwrites.page ? overwrites.page : (req.query.page ? req.query.page : '');
-    var limit = overwrites.limit ? overwrites.limit : (req.query.limit ? req.query.limit : '');
-    var searchType = overwrites.searchType ? overwrites.searchType : (req.query.searchType ? req.query.searchType : '');
-    var searchText = overwrites.searchText ? overwrites.searchText : (req.query.searchText ? req.query.searchText : '');
+    var page = overwrites.page
+      ? overwrites.page
+      : req.query.page
+      ? req.query.page
+      : "";
+    var limit = overwrites.limit
+      ? overwrites.limit
+      : req.query.limit
+      ? req.query.limit
+      : "";
+    var searchType = overwrites.searchType
+      ? overwrites.searchType
+      : req.query.searchType
+      ? req.query.searchType
+      : "";
+    var searchText = overwrites.searchText
+      ? overwrites.searchText
+      : req.query.searchText
+      ? req.query.searchText
+      : "";
 
-    if (page) queryArray.push('page=' + page);
-    if (limit) queryArray.push('limit=' + limit);
-    if (searchType) queryArray.push('searchType=' + searchType);
-    if (searchText) queryArray.push('searchText=' + searchText);
+    if (page) queryArray.push("page=" + page);
+    if (limit) queryArray.push("limit=" + limit);
+    if (searchType) queryArray.push("searchType=" + searchType);
+    if (searchText) queryArray.push("searchText=" + searchText);
 
-    if (queryArray.length > 0) queryString = (isAppended ? '&' : '?') + queryArray.join('&');
+    if (queryArray.length > 0)
+      queryString = (isAppended ? "&" : "?") + queryArray.join("&");
 
     return queryString;
-  }
+  };
   next();
-}
+};
 
-util.convertToTrees = function (array, idFieldName, parentIdFieldName, childrenFieldName) {
+util.convertToTrees = function (
+  array,
+  idFieldName,
+  parentIdFieldName,
+  childrenFieldName
+) {
   var cloned = array.slice();
 
   for (var i = cloned.length - 1; i > -1; i--) {
@@ -70,24 +92,22 @@ util.convertToTrees = function (array, idFieldName, parentIdFieldName, childrenF
 
         if (parent[childrenFieldName]) {
           parent[childrenFieldName].unshift(cloned[i]);
-        }
-        else {
+        } else {
           parent[childrenFieldName] = [cloned[i]];
         }
-
       }
       cloned.splice(i, 1);
     }
   }
 
   return cloned;
-}
+};
 
 util.bytesToSize = function (bytes) {
-  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes == 0) return '0 Byte';
+  var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes == 0) return "0 Byte";
   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-}
+  return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+};
 
 module.exports = util;
